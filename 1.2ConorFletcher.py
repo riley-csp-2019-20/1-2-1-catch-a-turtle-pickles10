e2f78cddb0b8492c015ad9c042b2dfc09905acd3
pickle <55500757+pickles10@users.noreply.github.com>
2019-10-22 11:36:01 -0400

    Add files via upload

---
+++ 1.2ConorFletcher.py
@@ -0,0 +1,23 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl 
+
+#-----game configuration----
+shape = "square"
+size = 5   
+color = "beige"
+
+#-----initialize turtle-----
+ham = trtl.Turtle(shape = shape)
+ham.color(color)
+ham.shapesize(size)
+
+
+
+#-----game functions--------
+
+
+#-----events----------------]wgsdhrrsdejgewghiueewguewghewgheuigheuwighewuighuwehgweughuewihgeuwighwiuehguhguiewhghghdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssjkssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+wn = trtl.Screen()
+wn.mainloop()
